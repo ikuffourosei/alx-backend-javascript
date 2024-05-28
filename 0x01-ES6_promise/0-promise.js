572e38982643590d58470d2111ccd35d2b9712cb
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-05-28 18:25:47 +0000

    javascript promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,12 @@
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        const num = Math.random();
-        if (num <= 0.5) {
-            resolve('Yay! it worked');
-        } else {
-            reject('Oops! Failed!');
-        }
-    });
+  return new Promise((resolve, reject) => {
+    const num = Math.random();
+    if (num <= 0.5) {
+      resolve('Yay! it worked');
+    } else {
+      reject(new Error('Oops! Failed!'));
+    }
+  });
 }
 
 module.exports = getResponseFromAPI;

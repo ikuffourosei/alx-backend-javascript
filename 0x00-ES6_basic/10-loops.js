99895a574dfd6afa7762938ea8b0fbb2e5a753d7
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-05-31 14:24:54 +0000

    ES6 BASICS

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (const [index, value] of array.entries()) {
-      array[index] = appendString + value;
-    }
-    return array;
+  for (const [index, value] of array.entries()) {
+    array[index] = appendString + value;
   }
-  
+  return array;
+}

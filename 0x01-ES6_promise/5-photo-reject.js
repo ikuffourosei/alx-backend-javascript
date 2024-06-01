5404b3975631e0634f155792d83bb56a89aa1bbb
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-06-01 13:24:59 +0000

    ES6 PROMISES

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
-    reject(new Error('fileName cannot be processed'));
+  return new Promise((resolve, reject) => {
+    reject(new Error(`${fileName} cannot be processed`));
   });
 }

11328f59e321b410536105b1a50ea86fc7ddef0c
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-06-01 13:57:28 +0000

    ES6 PROMISES

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,23 @@
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+  try {
+    const photoPromise = uploadPhoto();
+    const userPromise = createUser();
+
+    const [photo, user] = await Promise.all([photoPromise, userPromise]);
+
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    console.error('An error occurred:', error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
+
+export default asyncUploadUser;

57ccf0f0502c20c2f90f96e78cbcbe0932d10af7
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-06-04 21:30:10 +0000

    DATA MANIPULATION

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,13 @@
-export default function createInt8TypedArray(length, position, value) {
-  const typeArr = new ArrayBuffer(length);
-  const result = new Int8Array(typeArr);
-  if (position >= length) {
-    throw new Error('Position out of range');
+function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
   }
-  result[position] = value;
+
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+
+  return view;
 }
+
+export default createInt8TypedArray;

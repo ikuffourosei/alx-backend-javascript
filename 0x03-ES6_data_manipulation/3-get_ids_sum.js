66ed02a6158b2d56cb83e08ecf6be2e9934f238d
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-06-04 18:26:30 +0000

    DATA MANIPULATION

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,6 @@
 function getStudentIdsSum(list) {
-  
+  const newList = list.map((items) => items.id);
+  return newList.reduce((first, second) => first + second);
 }
 
 export default getStudentIdsSum;

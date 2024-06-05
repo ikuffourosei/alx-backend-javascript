708ec414f969408b39ce1caa7eb9045f43aba95d
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-06-05 02:21:26 +0000

    ES6 CLASSES

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,41 +1,38 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name === 'string') {
-      this._name = name;
-    }
-    if (typeof length === 'number') {
-      this._length = length;
-    }
-    if (Array.isArray(students) && students.every((student) => typeof student === 'string')) {
-      this._students = students;
-    }
+    this._name = typeof name === 'string' ? name : '';
+    this._length = typeof length === 'number' ? length : 0;
+    this._students = Array.isArray(students) ? students : [];
   }
 
   get name() {
     return this._name;
   }
 
-  set name(param) {
-    this._name = param;
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    }
   }
 
   get length() {
     return this._length;
   }
 
-  set length(param) {
-    this._length = param;
+  set length(value) {
+    if (typeof value === 'number') {
+      this._length = value;
+    }
   }
 
   get students() {
     return this._students;
   }
 
-  set students(param) {
-    if (Array.isArray(param) && param.every((student) => typeof student === 'string')) {
-      this._students = param;
+  set students(value) {
+    if (Array.isArray(value)) {
+      this._students = value;
     }
   }
 }
-
 export default HolbertonCourse;

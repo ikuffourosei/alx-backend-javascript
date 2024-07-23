da070ffacd50729c513ea53f24172787a0bf48a0
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-07-23 09:56:29 +0000

    Node Basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,11 @@
-const readline = require('node:readline');
-
-const read = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 console.log('Welcome to Holberton School, what is your name?');
 
-read.question('', (name) => {
-  const names = name.trim();
-  if (names) {
-    console.log(`Your name is: ${name}`);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
   }
-  read.close();
 });
-
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });

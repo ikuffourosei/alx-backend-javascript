55ed89a21ad1eccd5f5c8f8646399e8ecb1d3b2e
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-07-27 11:03:17 +0000

    Async ReadFile

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,38 +1,43 @@
-const fs = require('node:fs');
+const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
+    fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
         return;
       }
-      const newData = data.trim().split('\n');
-      // remove first line (header line)
-      newData.shift();
-      const students = {};
+
+      const lines = data.trim().split('\n');
+      const headers = lines.shift();
+
+      if (!headers || lines.length === 0) {
+        console.log('Number of students: 0');
+        resolve();
+        return;
+      }
+
+      const studentsByField = {};
       let totalStudents = 0;
 
-      newData.forEach((line) => {
-        if (line.trim().length > 0) {
-          const [firstname, , , field] = line.split(',');
+      lines.forEach((line) => {
+        const [firstname, , , field] = line.split(',');
 
-          if (!students[field]) {
-            students[field] = [];
+        if (field) {
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
           }
-          students[field].push(firstname);
+          studentsByField[field].push(firstname);
           totalStudents += 1;
         }
       });
 
       console.log(`Number of students: ${totalStudents}`);
-      for (const field in students) {
-        if (Object.prototype.hasOwnProperty.call(students, field)) {
-          const studentList = students[field].join(', ');
-          console.log(`Number of students in ${field}: ${students[field].length}. List: ${studentList}`);
-        }
+      for (const [field, students] of Object.entries(studentsByField)) {
+        console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
       }
-      resolve(true);
+
+      resolve();
     });
   });
 }
